f3f86f91de3f1a1b7c38af7af42c338daa1c0605
sotosv <sovasileiadis@uth.gr>
2021-05-05 15:44:52 +0300

    project files

--- 4_microbiomes/1_fetch_and_demultiplex/3_tbz_and_cobalamin_levels_a/2_demultiplexed_sequences/run_demux.sh
+++
@@ -1,4 +0,0 @@
-# obtain the computer processors
-MY_PROCS=20
-# call the demultiplexing pipeline
-./DemuxOwnBCsys_absPATH.sh demux_out /mnt/21B044BE29552220/sotos/TBZ/mconsort_tbz_degr/4_microbiomes/3_tbz_and_cobalamin_levels_a/2_demultiplexed_sequences/R1.fastq /mnt/21B044BE29552220/sotos/TBZ/mconsort_tbz_degr/4_microbiomes/3_tbz_and_cobalamin_levels_a/2_demultiplexed_sequences/R2.fastq bcfile.txt ${MY_PROCS}
--- 4_microbiomes/1_fetch_and_demultiplex/3_tbz_and_cobalamin_levels_b/2_demultiplexed_sequences/run_demux_pre.sh
+++
@@ -1,4 +0,0 @@
-# obtain the computer processors
-MY_PROCS=20
-# call the demultiplexing pipeline
-./DemuxOwnBCsys_absPATH.sh demux_out absolutepath/R1.fastq absolutepath/R2.fastq bcfile.txt ${MY_PROCS}
